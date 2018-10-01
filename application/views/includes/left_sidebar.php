3f13b5adc4f4976779a4a425f08f1625966ab562
sohanhumanpixel <sohan@team.humanpixel.com.au>
2018-10-01 18:43:13 +0530

    rolebased show menu

--- application/views/includes/left_sidebar.php
+++ application/views/includes/left_sidebar.php
@@ -1,7 +1,10 @@
 <div class="col-md-2">
 	<div class="sidebar content-box" style="display: block;">
 		<ul class="nav">
-		  <li class="current">
+		<?php
+		$userLogin = $this->session->userdata ( 'logged_in' );
+		if($userLogin['role_id'] == ROLE_ADMIN || $userLogin['role_id'] == ROLE_MANAGER){ ?>
+		 <li class="current">
 			<a href="<?php echo base_url() ?>dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
 		 </li>
 		 <li class="">
@@ -13,6 +16,29 @@
 		<li class="">
 			<a href="<?php echo base_url() ?>UserGroups/grouplist"><i class="glyphicon glyphicon-list"></i>Groups</a>
 		</li>
+		<li class="">
+			<a href="<?php echo base_url() ?>Projects/ProjectList"><i class="glyphicon glyphicon-list"></i>Project List</a>
+		</li>
+		<li class="">
+			<a href="<?php echo base_url() ?>TimeSheet/listtimesheet"><i class="glyphicon glyphicon-list"></i>Timesheet</a>
+		</li>
+		<?php }else{ ?>
+		 <li class="current">
+			<a href="<?php echo base_url() ?>Employee/dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
+		 </li>
+		 <li class="">
+			<a href="<?php echo base_url() ?>Employee/taskList"><i class="glyphicon glyphicon-plus"></i>Your Task </a>
+		 </li>
+		 <li class="">
+			<a href="<?php echo base_url() ?>Employee/grouplist"><i class="glyphicon glyphicon-plus"></i>Your Group </a>
+		 </li>
+		 <li class="">
+			<a href="<?php echo base_url() ?>TimeSheet/listtimesheet"><i class="glyphicon glyphicon-list"></i>Your Timesheet</a>
+		</li>
+		<?php
+		}
+		?>
+		
 		</ul>
 	</div>
 </div>
