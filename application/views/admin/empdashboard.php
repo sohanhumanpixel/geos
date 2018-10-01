7328ea39f62f516c958dab1005049f1e76bd2801
sohanhumanpixel <sohan@team.humanpixel.com.au>
2018-10-01 18:59:32 +0530

    emp dashboar @ 01-oct 2018

---
+++ application/views/admin/empdashboard.php
@@ -0,0 +1,33 @@
+<div class="page-content">
+	<div class="row">
+	<?php $this->load->view('includes/left_sidebar');?>
+		<div class="col-md-10">
+			<div class="content-box-large">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<h3>Welcome to Your Intranet!!</h3>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="col-lg-3 col-xs-6">
+						<div class="small-box bg-aqua">
+							<div class="inner">
+							  <h3>0</h3>
+							  <p>Total Your task</p>
+							</div>
+						  </div>
+					</div>
+					<div class="col-lg-3 col-xs-6">
+						<div class="small-box bg-green">
+							<div class="inner">
+							  <h3><?php echo $countGroup;?></h3>
+							  <p>Total Groups</p>
+							</div>
+						  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	</div>
+</div>
