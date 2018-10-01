4b2072d347b8ef28d94c6aeb9eacf019ab1e5f7c
sohanhumanpixel <sohan@team.humanpixel.com.au>
2018-10-01 18:40:19 +0530

    admin dabsboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,32 @@
 <div class="page-content">
-	<?php $this->load->view('includes/left_sidebar');?> 
+	<div class="row">
+		<?php $this->load->view('includes/left_sidebar');?> 
+		<div class="col-md-10">
+			<div class="content-box-large">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<h3>Welcome to Your Intranet!!</h3>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="col-lg-3 col-xs-6">
+						<div class="small-box bg-aqua">
+							<div class="inner">
+							  <h3>150</h3>
+							  <p>Total Users</p>
+							</div>
+						  </div>
+					</div>
+					<div class="col-lg-3 col-xs-6">
+						<div class="small-box bg-green">
+							<div class="inner">
+							  <h3>150</h3>
+							  <p>Total Groups</p>
+							</div>
+						  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
