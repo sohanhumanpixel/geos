77c9bb824c6d3e6c169891dc0efd9dfac27d2109
sohanhumanpixel <sohan@team.humanpixel.com.au>
2018-10-05 11:41:59 +0530

    add right sidebar@ 05-10-2018

---
+++ application/views/includes/right_sidebar.php
@@ -0,0 +1,24 @@
+<div class="col-md-2">
+	<div class="right sidebar content-box" style="display: block;">
+		<div class="panel-group">
+                <h4 class="panel-title light-bg">
+                   Quick Poll
+                </h4>
+                <ul class="list-group">
+
+				</ul>
+			  </div>
+			     <div class="panel-group light-bg">
+                <h4 class="panel-title">
+                   Look who's new
+                </h4>
+				<div class="views">
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			  </div>
+			  
+			     
+		
+		<!--</ul>-->
+	</div>
+</div>
