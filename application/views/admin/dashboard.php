e6df09d57a8f4fe8a18d72218f27615ab614f10c
sohanhumanpixel <sohan@team.humanpixel.com.au>
2018-10-17 19:10:57 +0530

    integrate calendar@16-10-2018

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -84,10 +84,40 @@
 				
 			</div>
 		    <?php } ?>
+			<div class="content-box-large employeecalendar">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<h3>Your Leave</h3>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="col-lg-12">
+						<div id="employeeCal"></div>
+					</div>
+				</div>
+			</div>
 		</div>
 		 
 		<?php $this->load->view('includes/right_sidebar');?> 
 	</div>
 </div>
-
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/frontend/fullcalendar/fullcalendar.min.css" />
+<script src="<?php echo base_url() ?>assets/frontend/fullcalendar/lib/moment.min.js"></script>
+<script src="<?php echo base_url() ?>assets/frontend/fullcalendar/fullcalendar.min.js"></script>
+<script src="<?php echo base_url() ?>assets/frontend/fullcalendar/gcal.js"></script>
+<script>
+	$(document).ready(function() {
+	$('#employeeCal').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      eventLimit: true, // allow "more" link when too many events
+        events: baseURL + 'Employee/ajax_getLeaveList',
+        selectable: true,
+        selectHelper: true
+    });
+});
+</script>
 <?php $this->load->view('includes/footer');?> 
